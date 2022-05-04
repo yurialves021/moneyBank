eb55cbf2ad9f1b476f53ac59049164519f17bd2a
Yuri Alves <yurialvesedf@gmail.com>
2022-05-04 10:46:27 -0300

    Implementação do sistema basico de autenticação

--- Funcionario/Diretor.js
+++ Funcionario/Diretor.js
@@ -3,5 +3,6 @@ import { Funcionario } from "./Funcionario.js";
 export class Diretor extends Funcionario {
     constructor(nome, salario, cpf) {
         super(nome, salario, cpf)
+        this._bonificacao = 2;
     }
 }
--- index.js
+++ index.js
@@ -2,16 +2,16 @@ import {Cliente} from "./Cliente.js";
 import { ContaCorrente } from "./Conta/ContaCorrente.js";
 import { ContaPoupanca } from "./Conta/ContaPoupanca.js";
 import { ContaSalario } from "./Conta/ContaSalario.js";
-
-const cliente1 = new Cliente("Yuri Alves", 88899977723);
-const contaCorrente = new ContaCorrente(cliente1,1001);
-const contaPoupanca = new ContaPoupanca(1500,cliente1,1001);
-const contaSalario = new ContaSalario(cliente1);
+import { Gerente }  from  "./Funcionario/Gerente.js";     
+import { Diretor }  from  "./Funcionario/Diretor.js";     
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
 
+const diretor = new Diretor ("Ricardo",10000,123456799);
+const gerente = new Gerente("Andre",5000,123456788);
 
-contaSalario.deposito(100);
-contaSalario.sacar(10);
+diretor.cadastrarSenha("123456");
 
-console.log(contaSalario);
+const estaLogado = SistemaAutenticacao.login(diretor,"123456");
 
+console.log(estaLogado);
